6e9460f1f636130382ff42f81b9fdc48d0a5f90d
Kegan Leatherow <leatherowke@gmail.com>
2022-04-16 16:16:43 -0500

    arrays.js, gitInfo.js, and sldc.js done

--- sldc.js
+++ sldc.js
@@ -6,11 +6,11 @@
 */
 
 // Analysis of Requirements
+// Planning
 // Design
+// Testing & Integration
 // Implementation
 // Maintain
-// Planning
-// Testing & Integration
 
 
 //////////////////PROBLEM 2////////////////////
@@ -18,4 +18,9 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the second step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = 'the client/customer defines a problem that needs to be solved'
+const design = 'in this step the software is made'
+const testingAndIntegrations = 'in this step QA does a check over the software to find any bugs or problems that may need to be fixed'
+const Implementation = 'the software is implemented and put into use'
+const Maintain = 'further maintenance of the software is preformed to keep it up to date and functioning properly'
