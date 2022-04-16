64d083ce101c0734dbd1a825565a302195ab7dd8
Kegan Leatherow <leatherowke@gmail.com>
2022-04-16 16:00:39 -0500

    arrays.js done

--- arrays.js
+++ arrays.js
@@ -5,10 +5,16 @@ const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
 
+let faveColorsCopy = faveColors.slice(0,3)
+console.log(faveColorsCopy)
+
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+
+faveColors.push("blue")
+console.log(faveColors)
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -20,6 +26,8 @@ const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
 
+let middleNums = numbers.slice(1,4)
+console.log(middleNums)
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -34,3 +42,11 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+
+const answers =[] ;
+for (i = 0; i < bigOrSmallArray.length; i++){
+ if(bigOrSmallArray[i]> 100){
+   answers.push('greater')
+ } 
+}
+ console.log(answers)
