3ef9dc7950035a2e57898a230bfd02362dd91078
Kegan Leatherow <leatherowke@gmail.com>
2022-04-16 20:01:27 -0500

    password.js done

--- password.js
+++ password.js
@@ -1,5 +1,21 @@
-console.log('welcome to the password validator tool!');
-const readline = require("readline");
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 
-  }
+
+reader.question('welcome to the password validator tool!  Enter Password: ', function(input){
+
+if(input.length > 10){
+  console.log('success!')
+}
+else{console.log('Too short!')
+}
+
+	// This line closes the connection to the command line interface.
+	reader.close()
+
+});
